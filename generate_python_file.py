6844e6298610e14ef48df3c7d7c6153a1ea74e52
uuboyscy <aegis12321@gmail.com>
2025-07-13 15:06:20 +0800

    fix: Update URL formatting in generate_python_file.py for better readability

--- generate_python_file.py
+++ generate_python_file.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://leetcode.com/problems/department-top-three-salaries/"
+URL = """
+https://leetcode.com/problems/department-top-three-salaries/
+""".strip()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
