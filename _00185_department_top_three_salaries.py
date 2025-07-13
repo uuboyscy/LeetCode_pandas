92ea3bb0200ce2a0802194646daad160d8fd67ad
uuboyscy <aegis12321@gmail.com>
2025-07-13 15:40:43 +0800

    185

---
+++ _00185_department_top_three_salaries.py
@@ -0,0 +1,137 @@
+"""
+Can you solve this real interview question? Department Top Three Salaries - Table: Employee
+
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference column) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+
+
+ 
+
+Table: Department
+
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of a department and its name.
+
+
+ 
+
+A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+
+Write a solution to find the employees who are high earners in each of the departments.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 85000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
+| 7  | Will  | 70000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Joe      | 85000  |
+| IT         | Randy    | 85000  |
+| IT         | Will     | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+Explanation: 
+In the IT department:
+- Max earns the highest unique salary
+- Both Randy and Joe earn the second-highest unique salary
+- Will earns the third-highest unique salary
+
+In the Sales department:
+- Henry earns the highest salary
+- Sam earns the second-highest salary
+- There is no third-highest salary as there are only two employees
+
+
+ 
+
+Constraints:
+
+ * There are no employees with the exact same name, salary and department.
+"""
+
+import pandas as pd
+
+
+def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    sorted_distinct_salary_department = (
+        employee
+        .drop_duplicates(["departmentId", "salary"])
+        .sort_values(["departmentId", "salary"], ascending=False)
+    )[
+        ["departmentId", "salary"]
+    ]
+
+    sorted_distinct_salary_department["row_num"] = sorted_distinct_salary_department.groupby(["departmentId"]).cumcount() + 1
+
+    sorted_distinct_salary_department = sorted_distinct_salary_department[
+        sorted_distinct_salary_department["row_num"] <= 3
+    ][
+        ["departmentId", "salary"]
+    ]
+
+    return sorted_distinct_salary_department.merge(  # type:ignore
+        employee,
+        how="left",
+        left_on=["departmentId", "salary"],
+        right_on=["departmentId", "salary"],
+    ).merge(
+        department,
+        how="left",
+        left_on="departmentId",
+        right_on="id",
+        suffixes=("_employee", "_department")
+    )[
+        ["name_department", "name_employee", "salary"]
+    ].rename(
+        columns={
+            "name_department":  "Department",
+            "name_employee": "Employee",
+            "salary": "Salary",
+        }
+    )
