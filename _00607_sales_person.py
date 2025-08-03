f53aabb9a878987833898fc9ec44a03ad0245df8
uuboyscy <aegis12321@gmail.com>
2025-08-03 21:28:52 +0800

    607

--- _00607_sales_person.py
+++ _00607_sales_person.py
@@ -121,3 +121,18 @@ def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.D
             ).query("name == 'RED'")["sales_id"]
         )
     ][["name"]]
+
+
+"""
+select
+    name
+from SalesPerson
+where sales_id not in (
+    select
+        sales_id
+    from Orders o
+    left join Company c
+    on o.com_id = c.com_id
+    where name = 'RED'
+)
+"""
