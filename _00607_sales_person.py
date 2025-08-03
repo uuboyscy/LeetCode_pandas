a8dc11c1f0202246dc9246bc415d3441044c1b25
uuboyscy <aegis12321@gmail.com>
2025-08-03 21:34:05 +0800

    607

--- _00607_sales_person.py
+++ _00607_sales_person.py
@@ -113,12 +113,11 @@ import pandas as pd
 def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     return sales_person[
         ~sales_person["sales_id"].isin(
-            orders.merge(
-                company,
-                how="left",
-                left_on="com_id",
-                right_on="com_id",
-            ).query("name == 'RED'")["sales_id"]
+            orders[
+                orders["com_id"].isin(
+                    company[company["name"] == "RED"]["com_id"]
+                )
+            ]["sales_id"]
         )
     ][["name"]]
 
